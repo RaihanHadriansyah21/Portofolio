a07e60e8c9b19b5b00e2efef81eba20a569c58ca
RaihanHadriansyah21 <raihanhadriansyah111@gmail.com>
2026-09-08 15:36:51 +0700

    Fine-tune lanyard card size to 2.4 for better precision

--- components/lanyard-showcase.jsx
+++ components/lanyard-showcase.jsx
@@ -60,7 +60,7 @@ export function LanyardShowcase({ locale }) {
           backImage="/react-bits/lanyard/reyy-pass-back.svg"
           lanyardImage="/react-bits/lanyard/reyy-band.svg"
           lanyardWidth={1.0}
-          cardScale={2.6}
+          cardScale={2.4}
           imageFit="cover"
         />
       ) : (
